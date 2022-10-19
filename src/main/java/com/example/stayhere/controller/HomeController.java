f39037c8cdbed4a4ce9aeb111298a710ef3f4098
namhyun99 <sshft@naver.com>
2022-10-19 15:23:03 +0900

    홈 컨트롤러 수정

--- src/main/java/com/example/stayhere/controller/HomeController.java
+++ src/main/java/com/example/stayhere/controller/HomeController.java
@@ -16,23 +16,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	
-	
-	 @RequestMapping(value = "/", method = RequestMethod.GET) public String
-	 home(Locale locale, Model model) {
-	 logger.info("Welcome home! The client locale is {}.", locale);
-	 
-	 Date date = new Date(); DateFormat dateFormat =
-	 DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-	 
-	 String formattedDate = dateFormat.format(date);
-	 
-	 model.addAttribute("serverTime", formattedDate );
-	 
-	 return "main"; }
-	 
-	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "main";
+	}
+
 }
